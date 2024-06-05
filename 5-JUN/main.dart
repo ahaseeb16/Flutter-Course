93c756372181639bacf45ea6f8f7ff817f094bfa
Your Name <you@example.com>
2024-06-05 22:02:17 +0500

    code

--- 5-JUN/main.dart
+++ 5-JUN/main.dart
@@ -1,7 +1,8 @@
 void main() {
   List list1 = [1, 2, 3, 4, "abc"];
-  print(list1);
+  print(list1[4]);
   //print(list1.reversed.toList());
-  print(list1.reversed.runtimeType);
-  print(list1.runtimeType);
+  //print(list1.reversed.runtimeType);
+  // print(list1.runtimeType);
+  print(list1.isNotEmpty);
 }
